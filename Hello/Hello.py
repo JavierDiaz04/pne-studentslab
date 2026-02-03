353c746be2fc276c57abd3c2a7b1e82389459141
javierdi <diazsanchez@gmail.com>
2026-02-03 16:48:38 +0100

    My first change to the READme.

--- Hello/Hello.py
+++ Hello/Hello.py
@@ -1,3 +1,7 @@
 print("This is my first program in PyCharm")
 print("Hello world")
 print("Let's do this")
+a = 5
+b = 6
+c = 7
+print(a*b/c)
---
+++ hello 2/S01/sum20.py
@@ -0,0 +1,7 @@
+i = 0
+c = 0
+while i < 20:
+    c = c + i
+    i = i + 1
+
+print(c)
