39dfa274b3749b3ee4e8a431dad6293c3ee41d15
javierdi <diazsanchez@gmail.com>
2026-03-03 16:35:33 +0100

    My first change to the READme.

---
+++ S08/e1.py
@@ -0,0 +1,2 @@
+mobile wifi 3/3/2026 Fuenlabrada 10.8.44.79
+Lab computer wire 3/3/2026 Fuenlabrada 212.128.255.96
---
+++ S08/e2.py
@@ -0,0 +1,6 @@
+Destination IP  Name(if known) Time
+212.128.255.96  l3208-pc32     2.77ms
+8.8.8.8         Google         2.96ms
+10.8.44.79      Mobile         97.4ms
+212.128.240.50  www.urjc.es    1.86ms
+140.82.121.3    www.github.com 35.4ms
